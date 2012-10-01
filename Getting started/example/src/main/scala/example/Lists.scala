bc4dd6985b6e23c7f8b26c76418ff2d459c91445
Uzi Landsmann <uzi.landsmann@gmail.com>
2012-10-01 06:30:21 +0200

    added my (Uzis) solution to example and week 1

--- Getting started/example/src/main/scala/example/Lists.scala
+++ Getting started/example/src/main/scala/example/Lists.scala
@@ -24,11 +24,9 @@ object Lists {
    * @return The sum of all elements in `xs`
    */
   def sum(xs: List[Int]): Int = {
-      if (xs.isEmpty)
-        0
-      else
-        xs.head + sum(xs.tail)
-  }
+    if (xs.isEmpty) 0
+    else xs.head + sum(xs.tail)
+}
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -44,15 +42,13 @@ object Lists {
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
   def max(xs: List[Int]): Int = {
-    if (xs.isEmpty) 
-      throw new java.util.NoSuchElementException  
-    
-    if (xs.tail.isEmpty)
-      xs.head
-    else
-      if (xs.head > max(xs.tail))
-        xs.head
-      else
-        max(xs.tail)   
-  }  
+
+    def bigger(x: Int, y: Int): Int = if (x > y) x else y
+
+    xs match {
+      case Nil => throw new NoSuchElementException
+      case x :: Nil => x
+      case x :: rest => bigger(x, max(rest))
+    }
+  }
 }
--- Week1/recfun/src/main/scala/recfun/Main.scala
+++ Week1/recfun/src/main/scala/recfun/Main.scala
@@ -15,10 +15,7 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if (r < 0) throw new IllegalArgumentException
-    if (c < 0 || c > r) throw new IllegalArgumentException
-
-    if (c == 0 || c == r) 1
+    if (c == 0 || r == 0 || c == r) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
@@ -26,18 +23,19 @@ object Main {
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-    
-    def balanceIter(chars: List[Char], open: Int): Boolean = {	    
-      if (open < 0) false
-      else if (chars.isEmpty) open == 0
+
+    def okIfEven(opens: Int, closed: Int, chars: List[Char]): Boolean = {
+      if (closed > opens) false
+      else if (chars.isEmpty) opens == closed
       else {
-	    if (chars.head == ')') balanceIter(chars.tail, open - 1)
-	    else if (chars.head == '(') balanceIter(chars.tail, open + 1)
-	    else balanceIter(chars.tail, open)
-	  }
+        okIfEven(
+          opens + (if (chars.head == '(') 1 else 0),
+          closed + (if (chars.head == ')') 1 else 0),
+          chars.tail)
+      }
     }
 
-    balanceIter(chars, 0)
+    okIfEven(0, 0, chars)
   }
 
   /**
@@ -46,9 +44,7 @@ object Main {
   def countChange(money: Int, coins: List[Int]): Int = {
       if (money < 0) 0
       else if (money == 0) 1
-      else {
-        if (coins.isEmpty) 0
-        else countChange(money - coins.head, coins) + countChange(money, coins.tail)
-      }
+      else if (coins.length == 0) 0
+      else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
