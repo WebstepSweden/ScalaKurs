46ee72ed617024401e2c9eb807aed51b44324378
Kristoffer Richardsson <kristoffer@richardsson.org>
2012-10-06 10:31:52 +0200

    Reworked union

--- Week3/objsets/src/main/scala/objsets/TweetSet.scala
+++ Week3/objsets/src/main/scala/objsets/TweetSet.scala
@@ -19,7 +19,11 @@ abstract class TweetSet {
   def filter(p: Tweet => Boolean): TweetSet = filter0(p, new Empty)
   def filter0(p: Tweet => Boolean, accu: TweetSet): TweetSet
 
-  def union(that: TweetSet): TweetSet
+  def union(that: TweetSet): TweetSet = union0(that, this)
+  def union0(that: TweetSet, accu: TweetSet): TweetSet = {
+    if (that.isEmpty) accu
+    else union0(that.tail, accu incl that.head)
+  }
 
   // Hint: the method "remove" on TweetSet will be very useful.
   def ascendingByRetweet: Trending = ascendingByRetweet0(new EmptyTrending)
@@ -63,7 +67,6 @@ abstract class TweetSet {
 class Empty extends TweetSet {
 
   def filter0(p: Tweet => Boolean, accu: TweetSet): TweetSet = accu
-  def union(that: TweetSet): TweetSet = that
 
   // The following methods are provided for you, and do not have to be changed
   // -------------------------------------------------------------------------
@@ -84,10 +87,6 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     else tmp
   }
   
-  def union(that: TweetSet): TweetSet = {
-    ((left union right) union that) incl elem
-  }
-
   // The following methods are provided for you, and do not have to be changed
   // -------------------------------------------------------------------------
   def contains(x: Tweet): Boolean =
@@ -153,12 +152,9 @@ object GoogleVsApple {
   
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-//  println("Start reading 1")
   val googleTweets: TweetSet = TweetReader.allTweets.filter(keywordFilter(google))
-//  println("Start reading 2")
 
   val appleTweets: TweetSet = TweetReader.allTweets.filter(keywordFilter(apple))
-//  println("Done reading")
   
   def keywordFilter(keywords: List[String]): Tweet => Boolean = {
     tweet => keywords.exists(tweet.text.contains(_))
