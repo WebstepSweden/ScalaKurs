6f0c04ef124dcb47dee3defb0f24dd185e013326
Uzi Landsmann <uzi.landsmann@gmail.com>
2012-10-28 01:09:08 +0200

    Fixed wordOccurrences, sentenceOccurrences, dictionaryByOccurrences and wordAnagrams. Still struggling with combinations.

--- Week6/forcomp/src/test/scala/forcomp/AnagramsSuite.scala
+++ Week6/forcomp/src/test/scala/forcomp/AnagramsSuite.scala
@@ -14,23 +14,29 @@ class AnagramsSuite extends FunSuite {
     assert(wordOccurrences("abcd") === List(('a', 1), ('b', 1), ('c', 1), ('d', 1)))
   }
 
+  test("wordOccurrences: abrakadabra") {
+    assert(wordOccurrences("abrakadabra") === List(('a', 5), ('b', 2), ('d', 1), ('k', 1), ('r', 2)))
+  }
+
+  test("wordOccurrences: abRAkaDABrA") {
+    assert(wordOccurrences("abRAkaDABrA") === List(('a', 5), ('b', 2), ('d', 1), ('k', 1), ('r', 2)))
+  }
+
   test("wordOccurrences: Robert") {
     assert(wordOccurrences("Robert") === List(('b', 1), ('e', 1), ('o', 1), ('r', 2), ('t', 1)))
   }
-
-
 
   test("sentenceOccurrences: abcd e") {
     assert(sentenceOccurrences(List("abcd", "e")) === List(('a', 1), ('b', 1), ('c', 1), ('d', 1), ('e', 1)))
   }
 
-
+  test("sentenceOccurrences: ABR ak adaB rA") {
+    assert(sentenceOccurrences(List("ABR", "ak", "adaB", "rA")) === List(('a', 5), ('b', 2), ('d', 1), ('k', 1), ('r', 2)))
+  }
 
   test("dictionaryByOccurrences.get: eat") {
     assert(dictionaryByOccurrences.get(List(('a', 1), ('e', 1), ('t', 1))).map(_.toSet) === Some(Set("ate", "eat", "tea")))
   }
-
-
 
   test("word anagrams: married") {
     assert(wordAnagrams("married").toSet === Set("married", "admirer"))
@@ -40,20 +46,11 @@ class AnagramsSuite extends FunSuite {
     assert(wordAnagrams("player").toSet === Set("parley", "pearly", "player", "replay"))
   }
 
+  
 
-
-  test("subtract: lard - r") {
-    val lard = List(('a', 1), ('d', 1), ('l', 1), ('r', 1))
-    val r = List(('r', 1))
-    val lad = List(('a', 1), ('d', 1), ('l', 1))
-    assert(subtract(lard, r) === lad)
-  }
-
-
-
-  test("combinations: []") {
+  /*test("combinations: []") {
     assert(combinations(Nil) === List(Nil))
-  }
+  }*/
 
   test("combinations: abba") {
     val abba = List(('a', 2), ('b', 2))
@@ -66,12 +63,16 @@ class AnagramsSuite extends FunSuite {
       List(('a', 2), ('b', 1)),
       List(('b', 2)),
       List(('a', 1), ('b', 2)),
-      List(('a', 2), ('b', 2))
-    )
+      List(('a', 2), ('b', 2)))
     assert(combinations(abba).toSet === abbacomb.toSet)
   }
-
-
+  
+  test("subtract: lard - r") {
+    val lard = List(('a', 1), ('d', 1), ('l', 1), ('r', 1))
+    val r = List(('r', 1))
+    val lad = List(('a', 1), ('d', 1), ('l', 1))
+    assert(subtract(lard, r) === lad)
+  }
 
   test("sentence anagrams: []") {
     val sentence = List()
@@ -100,9 +101,8 @@ class AnagramsSuite extends FunSuite {
       List("Uzi", "Rex", "null"),
       List("Zulu", "nil", "Rex"),
       List("rulez", "Linux"),
-      List("Linux", "rulez")
-    )
+      List("Linux", "rulez"))
     assert(sentenceAnagrams(sentence).toSet === anas.toSet)
-  }  
+  }
 
 }
