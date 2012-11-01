7ce9c99a5cc559c273fa180235b47ddf29a12c91
Kristoffer Richardsson <kristoffer@richardsson.org>
2012-11-01 19:36:48 +0100

    StringParserTerrain OK

--- Week7/streams/src/main/scala/streams/StringParserTerrain.scala
+++ Week7/streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,9 +52,15 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = (pos: Pos) => {
+    val Pos(x, y) = pos
 
-  /**
+    def insideVector = (x >= 0 && x < levelVector.length && y >= 0 && y < levelVector(0).length)
+
+    insideVector && levelVector(x)(y) != '-'
+  }
+
+    /**
    * This function should return the position of character `c` in the
    * terrain described by `levelVector`. You can assume that the `c`
    * appears exactly once in the terrain.
@@ -62,7 +68,11 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val x = levelVector.indexWhere(_.contains(c))
+    val y = levelVector(x).indexOf(c)
+    Pos(x, y)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
